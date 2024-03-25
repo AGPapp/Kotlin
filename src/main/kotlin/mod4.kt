d4cfb05b5651bfc1f765b922bd9e25694204d70e
AGPopov <AGPopov@appline.ru>
2024-03-25 18:21:00 +0300

    Модуль 4 задание 18

--- src/main/kotlin/mod4.kt
+++ src/main/kotlin/mod4.kt
@@ -1,15 +1,12 @@
 
 fun main() {
-    var air: Aircraft = Aircraft();
-    air.tank = 2000
-    println("Средний расход на 1 км - ${air.raskhod} литров.")
+    var air: Air = Air("g4545", 5000, 2000);
+
+    println("Средний расход на 1 км - ${air.raskhod} л.")
 }
 
-class Aircraft {
-    var num: String = "Y1578";
-    var maxLength: Int = 10000
-    var tank: Int = 1000
-    var raskhod:Any = 0.0
+class Air(var num:String, var maxLength:Int,var tank:Int){
+    var raskhod: Any = 0.0
         get() {
             return maxLength / tank
         }
