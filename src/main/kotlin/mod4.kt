35ff1aca852ea9262a3efb995fec668658c82c00
AGPopov <AGPopov@appline.ru>
2024-03-26 16:54:58 +0300

    Модуль 4 задание 20

--- src/main/kotlin/mod4.kt
+++ src/main/kotlin/mod4.kt
@@ -1,11 +1,13 @@
 
 fun main() {
-    var air: Air = Air("g4545", 5000, 2000);
+    var air: Aircraft = Aircraft("g4545", 5000, 2000);
 
     println("Средний расход на 1 км - ${air.raskhod} л.")
+    var bo=Boeing747("dsfg",123,232,500)
+    println(bo.capacity)
 }
 
-open class Air(var num:String, var maxLength:Int,var tank:Int){
+open class Aircraft(var num:String, var maxLength:Int,var tank:Int){
     var raskhod: Any = 0.0
         get() {
             return maxLength / tank
@@ -13,7 +15,9 @@ open class Air(var num:String, var maxLength:Int,var tank:Int){
 
 
 }
-class Boeing747(num: String, maxLength: Int, tank: Int): Air(num,maxLength, tank) {
-    var capacity: Int = 0
+interface Passenger {
+    var capacity: Int
+}
 
+class Boeing747(num: String, maxLength: Int, tank: Int, override var capacity:Int):Aircraft(num, maxLength, tank),Passenger{
 }
