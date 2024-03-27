32150b09be774d481c96dc784e393386a6c51de2
AGPopov <AGPopov@appline.ru>
2024-03-27 11:54:08 +0300

    Модуль 4 задание 22

--- src/main/kotlin/mod4.kt
+++ src/main/kotlin/mod4.kt
@@ -1,20 +1,30 @@
 
 fun main() {
-    var bo=Boeing747("dsfg",123,232,500)
-    println(bo.capacity)
+    var bo=Boeing747("ds34563",10000,25000,500)
+    println(bo.printInfo())
 }
 
-abstract class Aircraft(var num:String, var maxLength:Int,var tank:Int){
-    var raskhod: Any = 0.0
+abstract class Aircraft(protected var num:String, protected var maxLength:Int, protected var tank:Int){
+    private var x = maxLength.toDouble()
+    private var y = tank.toDouble()
+    var raskhod = 0.0
         get() {
-            return maxLength / tank
-        }
-
-
+            return (x / y)*100
+           }
+    open fun printInfo(){
+     println("Номер судна: $num; \nДальность полета: $maxLength; \nВместимость баков: $tank; \nРасход л/100 км: $raskhod;")
+          }
 }
+
+
 interface Passenger {
     var capacity: Int
 }
 
-class Boeing747(num: String, maxLength: Int, tank: Int, override var capacity:Int):Aircraft(num, maxLength, tank),Passenger{
+
+class Boeing747(num: String, maxLength:Int, tank:Int, override var capacity:Int):Aircraft(num, maxLength, tank),Passenger{
+    override fun printInfo() {
+        super.printInfo()
+        println("Вместимость пассажиров: $capacity;")
+    }
 }
